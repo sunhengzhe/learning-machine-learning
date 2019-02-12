162cae5ce9e4e88726c1e6193179ad5184186b6e
Hengzhe Sun <hengzhe.sun@thoughtworks.com>
2019-02-12 19:40:09 +0800

    support multiple variables

--- src/routes/linear-regression/index.ts
+++ src/routes/linear-regression/index.ts
@@ -1,5 +1,9 @@
 import * as express from "express";
 
+interface IThetaResp {
+    [key: string]: number;
+}
+
 const router = express.Router();
 
 const hypothesis = (coords: number[], thetas: number[]): number => {
@@ -68,8 +72,8 @@ router.get("/single-variable", (req: express.Request, res: express.Response) => 
       if (isStopLearning || Date.now() - startTime > 10000) {
           clearInterval(interval);
           res.send({
-              theta0: theta0.toFixed(2),
-              theta1: theta1.toFixed(2)
+              theta0: Number(theta0.toFixed(2)),
+              theta1: Number(theta1.toFixed(2))
           });
       }
 
@@ -77,5 +81,55 @@ router.get("/single-variable", (req: express.Request, res: express.Response) => 
       theta1 = temp1;
   });
 });
+
+router.get("/multiple-variables", (req: express.Request, res: express.Response) => {
+    const pointsInStr: string = req.query.points;
+    const learningRate = 0.05;
+
+    let points: Array<[number, number]> = [];
+
+    try {
+        points = JSON.parse(pointsInStr);
+    } catch (e) {
+        res.status(400).send("invalid points");
+        return;
+    }
+
+    if (!Array.isArray(points)) {
+        res.status(400).send("invalid points");
+        return;
+    }
+
+    if (points.length > 100) {
+        res.status(400).send("too many points");
+        return;
+    }
+
+    const thetas = [...new Array(4)].map(() => 0);
+
+    const startTime = Date.now();
+    const interval = setInterval(() => {
+        const tempThetas = calculateTheta(learningRate, points, thetas);
+
+        // tslint:disable-next-line:no-console
+        console.log(...tempThetas);
+
+        // less than 10e-3
+        const isStopLearning = thetas.every((theta, i) => Math.abs(theta - tempThetas[i]) < theta * 0.001);
+
+        if (isStopLearning || Date.now() - startTime > 10000) {
+            clearInterval(interval);
+
+            const resp: IThetaResp = {};
+            thetas.forEach((theta, i) => {
+                resp[`theta${i}`] = Number(theta.toFixed(2));
+            });
+
+            res.send(resp);
+        }
+
+        Object.assign(thetas, tempThetas);
+    });
+  });
 
 export default router;
